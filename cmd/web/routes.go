dae4ce50dafb9c7f8cc84070c944fdcefd11c190
Alireza Nasri <gg.nasri@gmail.com>
2024-08-21 12:49:22 +0200

    adding images and setting fileserver

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,6 +25,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fs := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fs))
+
 	return mux
 
 }
