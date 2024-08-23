62fb83293da6417a7fdcb0127135e2f963806b04
Alireza Nasri <gg.nasri@gmail.com>
2024-08-23 10:21:19 +0200

    Pushing the static assests

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -72,6 +72,36 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// Reservation renders the make-reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl.html", &models.TemplateData{})
+
+}
+
+// Generals renders the room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl.html", &models.TemplateData{})
+
+}
+
+// Majors renders the room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl.html", &models.TemplateData{})
+
+}
+
+// Availability renders the search availability
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl.html", &models.TemplateData{})
+
+}
+
+// Contact renders the search availability
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl.html", &models.TemplateData{})
+
+}
+
 // Addvalues returns the sum of values
 func AddValues(x, y int) int {
 	return x + y
